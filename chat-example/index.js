798de598608a3407998026cc95e40868658666d8
subhanaali <asubhana61@gmail.com>
2025-05-22 19:44:36 +0500

    Update index.js

--- chat-example/index.js
+++ chat-example/index.js
@@ -5,20 +5,8 @@ import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
-import { availableParallelism } from 'node:os';
-import cluster from 'node:cluster';
-import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
 
-if (cluster.isPrimary) {
-  const numCPUs = availableParallelism();
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork({
-      PORT: 3000 + i
-    });
-  }
-
-  setupPrimary();
-} else {
+(async () => {
   const db = await open({
     filename: 'chat.db',
     driver: sqlite3.Database
@@ -35,8 +23,7 @@ if (cluster.isPrimary) {
   const app = express();
   const server = createServer(app);
   const io = new Server(server, {
-    connectionStateRecovery: {},
-    adapter: createAdapter()
+    connectionStateRecovery: {}
   });
 
   const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -51,7 +38,7 @@ if (cluster.isPrimary) {
       try {
         result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset);
       } catch (e) {
-        if (e.errno === 19 /* SQLITE_CONSTRAINT */ ) {
+        if (e.errno === 19 /* SQLITE_CONSTRAINT */) {
           callback();
         } else {
           // nothing to do, just let the client retry
@@ -64,21 +51,22 @@ if (cluster.isPrimary) {
 
     if (!socket.recovered) {
       try {
-        await db.each('SELECT id, content FROM messages WHERE id > ?',
+        await db.each(
+          'SELECT id, content FROM messages WHERE id > ?',
           [socket.handshake.auth.serverOffset || 0],
           (_err, row) => {
             socket.emit('chat message', row.content, row.id);
           }
-        )
+        );
       } catch (e) {
         // something went wrong
       }
     }
   });
 
-  const port = process.env.PORT;
+  const port = process.env.PORT || 3000;
 
   server.listen(port, () => {
     console.log(`server running at http://localhost:${port}`);
   });
-}
+})();
